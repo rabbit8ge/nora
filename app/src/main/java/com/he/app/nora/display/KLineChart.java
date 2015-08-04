8d4a8b136d59c5bfd4a0bd9ef7a1d95bd1c4adfd
orinzhang <orinzhang@sina.com>
2015-08-04 22:05:35 +0800

    add MinuteLineChart

---
+++ app/src/main/java/com/he/app/nora/display/KLineChart.java
@@ -0,0 +1,25 @@
+package com.he.app.nora.display;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import cn.limc.androidcharts.view.MACandleStickChart;
+
+/**
+ * Created by zy on 8/4/2015.
+ */
+public class KLineChart extends MACandleStickChart {
+
+    public KLineChart(Context context) {
+        super(context);
+    }
+
+    public KLineChart(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public KLineChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
